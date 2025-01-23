3ff422308efe6bb1b133b51e6dc63808bb7c2939
navarchus <kk4atc@gmail.com>
2025-01-23 14:52:20 -0500

    update lut to handle whitespace and comments

--- src/lut.lua
+++ src/lut.lua
@@ -2,25 +2,38 @@ local function defaulthandler(self, _line)
     return({["startDir"]=self["start_dir"], ["angles"]=self["angles"]})
 end
 
+local function nilhandler(self, _line)
+    return nil
+end
+
 local hextable = {
     ["minds-reflection"]={
-        ["match_pattern"]="[%s]*Mind's Reflection[%s]*",
+        ["match_pattern"]="Mind's Reflection",
         ["start_dir"]="NORTH_EAST",
         ["angles"]="qaq",
         ["handler"]=defaulthandler
     },
     ["introspection"]={
-        ["match_pattern"]="[%s]*{[%s]*",
+        ["match_pattern"]="{",
         ["start_dir"]="NORTH_EAST",
         ["angles"]="qqq",
         ["handler"]=defaulthandler
     },
     ["retrospection"]={
-        ["match_pattern"]="[%s]*}[%s]*",
+        ["match_pattern"]="}",
         ["start_dir"]="NORTH_EAST",
         ["angles"]="eee",
         ["handler"]=defaulthandler
     },
+    --must be last
+    ["whitespace"]={
+        ["match_pattern"]="[%s]*",
+        ["handler"]=nilhandler
+    },
+    ["comment"]={
+        ["match_pattern"]="//.*\n",
+        ["handler"]=nilhandler
+    }
 }
 
 return {hextable = hextable}
