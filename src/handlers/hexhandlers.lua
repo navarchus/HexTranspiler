7e4ef6a8e34dbf302ac5114e96e4b749dcc946ed
navarchus <kk4atc@gmail.com>
2025-09-08 20:09:28 -0400

    use one handlers file

--- src/handlers/hexhandlers.lua
+++ src/handlers/hexhandlers.lua
@@ -275,6 +275,47 @@ local function numhandler(self, match, line_num)
     error("line " .. line_num .. ": " .. "Could not create number via decompositon!", 0)
 end
 
+-- The following are specific to Hexical
+local function nepthyshandler(self, match, line_num)
+    local num = tonumber(string.match(match, "Nephthys's Gambit:[%s]*([%d]+)"))
+    if num == nil or num < 0 then
+        error("Line: "..line_num.." Nepthys's Gambit must have a numerical value greater than or equal to 0")
+    end
+    local startDir = "SOUTH_EAST"
+    local angles = "deaqq"
+
+    for i = 1, num, 1 do
+        if i == 1 then
+            angles = angles.."d"
+        elseif i % 2 == 0 then
+            angles = angles.."q"
+        else
+            angles = angles.."e"
+        end
+    end
+
+    return {{["startDir"]=startDir, ["angles"]=angles}}
+end
+
+local function sehkmethandler(self, match, line_num)
+    local num = tonumber(string.match(match, "Sehkmet's Gambit:[%s]*([%d]+)"))
+    if num == nil or num < 0 then
+        error("Line: "..line_num.." Sehkmet's Gambit must have a numerical value greater than or equal to 0")
+    end
+    local startDir = "SOUTH_WEST"
+    local angles = "qaqdd"
+
+    for i = 1, num, 1 do
+        if i % 2 == 0 then
+            angles = angles.."e"
+        else
+            angles = angles.."q"
+        end
+    end
+
+    return {{["startDir"]=startDir, ["angles"]=angles}}
+end
+
 return {
     numhandler = numhandler,
     greatspellhandler = greatspellhandler,
@@ -283,5 +324,7 @@ return {
         definehandler,
     nilhandler = nilhandler,
     bookkeeperhandler = bookkeeperhandler,
-    includehandler = includehandler
+    includehandler = includehandler,
+    nepthyshandler = nepthyshandler,
+    sehkmethandler = sehkmethandler
 }
